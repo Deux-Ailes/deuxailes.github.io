33c82410ecac7e84a54124702f936990658ec99f
Deux Ailes <40036820+Deux-Ailes@users.noreply.github.com>
2023-01-09 21:50:58 +0000

    Pp bg

--- src/components/about/AboutMe.vue
+++ src/components/about/AboutMe.vue
@@ -28,7 +28,7 @@ export default {
 		<!-- About profile image -->
 		<div class="w-full sm:w-1/4 mb-7 sm:mb-0">
 			<img
-				src="@/assets/images/profilPic.jpeg"
+				src="@/assets/images/P1030410.JPG"
 				class="rounded-xl w-96"
 				alt=""
 			/>
